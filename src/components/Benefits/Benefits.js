ed6845c445c158a812eaef546f08250474c1e0d3
AndreewMelnik <andreew.mel@gmail.com>
2022-07-22 18:23:35 +0300

    .

--- src/components/Benefits/Benefits.js
+++ src/components/Benefits/Benefits.js
@@ -3,8 +3,15 @@ import './Benefits.css';
 function Benefits() {
   return (
 <div className="benefits">
-    <div className="wrapper__main">
-        <h1 className="title">Benefits of Odigo</h1>
+    <div className="wrapper">
+        <h1 className="benefits__title">Benefits of Odigo</h1>
+        <div className="benefits__cards">
+            <div class="benefits__grid">
+                <div class="benefits__card">A</div>
+                <div class="benefits__card">B</div>
+                <div class="benefits__card">C</div>
+            </div>
+        </div>
     </div>
 </div>
 
